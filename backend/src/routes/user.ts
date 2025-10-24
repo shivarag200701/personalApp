5612fbb3569d85921dd0bed7822e5c322d1053b5
Shiva Raghav <shivaraghav200701@gmail.com>
2025-10-24 14:42:20 -0500

    Added session based user storage

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,13 +2,56 @@ import express from "express";
 import { success, z } from "zod";
 const userRouter = express();
 import prisma from "../db/index.js";
+import { createClient } from "redis";
+import { RedisStore } from "connect-redis";
+import session from "express-session";
+import dotenv from "dotenv";
 
-export default userRouter;
+dotenv.config();
 
 const userSchema = z.object({
   username: z.string().min(3, "less than 3 letters"),
   password: z.string(),
 });
+
+const redisConnectionString = process.env.REDIS_URL || "";
+const secretString = process.env.SESSION_SECRET || "";
+
+//upstash client
+const redisClient = createClient({
+  url: redisConnectionString,
+});
+
+//creating store for client to talk with session
+const redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "upstash client",
+});
+
+redisClient
+  .connect()
+  .catch((error) => console.error("unable to connect to redis client", error));
+
+redisClient.on("connect", () => console.log("connected to upstash"));
+redisClient.on("error", (error) =>
+  console.error("Error connecting to upstash", error)
+);
+
+//session middleware
+userRouter.use(
+  session({
+    store: redisStore,
+    secret: secretString,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: "auto", // Use 'true' in production with HTTPS
+      maxAge: 1000 * 60 * 60 * 24, // Session expiration time (24 hours)
+    },
+  })
+);
+
+userRouter.use(express.json());
 
 type user = z.infer<typeof userSchema>;
 
@@ -35,13 +78,15 @@ userRouter.post("/signup", async (req, res) => {
       });
     }
 
-    await prisma.user.create({
+    const newUser = await prisma.user.create({
       data: {
         username,
         password,
       },
     });
-    return res.status(200).json({
+    //create session for user
+    req.session.userId = newUser.id;
+    return res.status(201).json({
       msg: "user created sucessfully",
     });
   } catch (error) {
@@ -80,6 +125,8 @@ userRouter.post("/signin", async (req, res) => {
         msg: "Please Enter a Valid password",
       });
     }
+
+    req.session.userId = user.id;
     return res.status(200).json({
       msg: "Logged in successfully",
     });
@@ -90,3 +137,5 @@ userRouter.post("/signin", async (req, res) => {
     });
   }
 });
+
+export default userRouter;
---
+++ backend/src/types.d.ts
@@ -0,0 +1,9 @@
+// types.d.ts
+
+import "express-session";
+
+declare module "express-session" {
+  interface SessionData {
+    userId: number;
+  }
+}

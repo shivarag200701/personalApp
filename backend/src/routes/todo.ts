f5fa08e9ac1d8c1cab490727f5880dd0241e6640
Shiva Raghav <shivaraghav200701@gmail.com>
2025-10-24 16:13:50 -0500

    Added post todo route handler

--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -1,21 +1,62 @@
 import express from "express";
 import { z } from "zod";
 import { requireLogin } from "../middleware.js";
+import prisma from "../db/index.js";
+import { Priority } from "@prisma/client";
+import { CompleteAt } from "@prisma/client";
 
 const todoRouter = express();
 
 const todoSchema = z.object({
   title: z.string(),
   description: z.string(),
-  priority: z.string(),
-  completeAt: z.string(),
+  priority: z.enum(Priority),
+  completeAt: z.enum(CompleteAt),
   category: z.string(),
 });
 
-todoRouter.post("/", requireLogin, (req, res) => {
-  res.json({
-    msg: "You are logged in",
-  });
+todoRouter.post("/", requireLogin, async (req, res) => {
+  const { data, success, error } = todoSchema.safeParse(req.body);
+  if (!success) {
+    return res.status(400).json({
+      msg: "Send proper data",
+      error,
+    });
+  }
+
+  const userId = req.session.userId;
+  if (!userId) {
+    return res.status(401).json({
+      msg: "unauthorized",
+    });
+  }
+  const { title, description, priority, completeAt, category } = data;
+
+  try {
+    const todo = await prisma.todo.create({
+      data: {
+        title,
+        description,
+        priority,
+        completeAt,
+        category,
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+
+    return res.status(200).json({
+      msg: "Todo added sucessfully",
+    });
+  } catch (error) {
+    console.error("Error while adding todo", error);
+    return res.status(500).json({
+      msg: "internal Server Error",
+    });
+  }
 });
 
 export default todoRouter;

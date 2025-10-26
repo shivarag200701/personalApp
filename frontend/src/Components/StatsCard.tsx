b283f25caddbeb16eb08766db328856ec4119358
Shiva Raghav <shivaraghav200701@gmail.com>
2025-10-26 08:59:50 -0500

    feat: common folder

--- frontend/src/Components/StatsCard.tsx
+++ frontend/src/Components/StatsCard.tsx
@@ -4,20 +4,20 @@ import { ListTodo } from "lucide-react";
 interface statsProps {
   label: string;
   icon: LucideIcon;
-  value: string;
+  value: number;
   trend: string;
 }
 
 const StatsCard = ({ label, icon, value, trend }: statsProps) => {
   return (
-    <div className=" grid grid-cols-2 max-w-[200px] shadow-md p-6 rounded-xl bg-[#1B1B1E] hover:opacity-90 transition-opacity">
+    <div className=" grid grid-cols-2 w-full border border-gray-800 shadow-md p-6 rounded-xl bg-[#1B1B1E] hover:opacity-90 transition-opacity">
       <div className="flex-col">
         <div className="text-[#A2A2A9]">Total Tasks</div>
-        <div className="text-[#F2F2F2] text-4xl font-bold">7</div>
+        <div className="text-[#F2F2F2] text-4xl font-bold">{value}</div>
         <div className="text-[#A2A2A9] text-[12px]">6 active</div>
       </div>
-      <div className="flex justify-center items-center">
-        <div className="bg-gradient-to-b from-[#4b3a75] text-blue-500 to-[#2a243a] p-4 rounded-2xl self-start flex items-center justify-center w-auto justify-self-start">
+      <div className="flex justify-center items-center relative">
+        <div className="bg-gradient-to-b from-[#4b3a75] text-blue-500 to-[#2a243a] p-3 rounded-2xl flex absolute top-0 right-0 ">
           <ListTodo />
         </div>
       </div>
--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -1,16 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AppBar from "../Components/AppBar";
 import axios from "axios";
 import StatsCard from "../Components/StatsCard";
 
 const Dashboard = () => {
+  const [totalTodoCount, setTotalCount] = useState(0);
   useEffect(() => {
     async function fetchTodo() {
       try {
         const res = await axios.get("/api/v1/todo/", {
           withCredentials: true,
         });
-        console.log(res.data);
+        const todos = res.data.todos;
+        console.log(todos);
+
+        const totalTodos = todos.length;
+        console.log(totalTodos);
+
+        setTotalCount(totalTodos);
       } catch (error) {
         console.log(error);
       }
@@ -18,12 +25,14 @@ const Dashboard = () => {
     fetchTodo();
   }, []);
   return (
-    <div className="h-screen bg-[#131315]">
+    <div className="h-screen bg-[#131315] max-w-6xl mx-auto p-4 md:p-8">
       <AppBar />
-      <StatsCard />
-      <StatsCard />
-      <StatsCard />
-      <StatsCard />
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <StatsCard value={totalTodoCount} />
+        <StatsCard />
+        <StatsCard />
+        <StatsCard />
+      </div>
     </div>
   );
 };

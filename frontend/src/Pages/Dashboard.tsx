00caba56d78e31ee021c6f7fdeb52acc52b8746b
Shiva Raghav <shivaraghav200701@gmail.com>
2025-11-14 08:04:21 -0600

    added max size of dashboard page

--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -177,7 +177,7 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className="min-h-screen bg-[#131315] max-w-6xl mx-auto p-4 md:p-8 pb-12">
+      <div className="min-h-screen bg-[#131315] max-w-7xl mx-auto p-4 md:p-8 pb-12">
         <div className="mb-8">
           <AppBar />
         </div>

09651f3f77c9397a1d4c1dc237598e084dd4fa6f
Shiva Raghav <shivaraghav200701@gmail.com>
2025-12-13 15:37:04 +0530

    fixed time zone conversions

--- frontend/src/Components/TodayView.tsx
+++ frontend/src/Components/TodayView.tsx
@@ -34,6 +34,7 @@ const TodayView = ({
       (todo) => isToday(todo?.completeAt) && todo?.completed == false
     );
   }, [todos]);
+  console.log("todayTodos", todayTodos);
 
   const todayCompletedTodos = useMemo(() => {
     return todos.filter(
